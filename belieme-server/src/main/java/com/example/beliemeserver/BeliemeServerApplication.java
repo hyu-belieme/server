fb2e8cefc9d167ed48cf2a8dd84ca8ad720a9c94
seokhwan <huan99@hanmail.net>
2022-04-21 17:02:19 +0900

    feat: add stuff custom table cell

---
+++ belieme-server/src/main/java/com/example/beliemeserver/BeliemeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.beliemeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeliemeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeliemeServerApplication.class, args);
+	}
+
+}
---
+++ belieme-server/src/test/java/com/example/beliemeserver/BeliemeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beliemeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeliemeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
