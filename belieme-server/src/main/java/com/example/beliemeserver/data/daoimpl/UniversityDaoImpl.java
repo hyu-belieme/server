13b5f5b792064184e28dbad08560b71f91a382c7
ESeokhwan <huan99@hanmail.net>
2022-12-31 00:24:38 +0900

    feat: add dao of university

---
+++ belieme-server/src/main/java/com/example/beliemeserver/data/daoimpl/UniversityDaoImpl.java
@@ -0,0 +1,78 @@
+package com.example.beliemeserver.data.daoimpl;
+
+import com.example.beliemeserver.data.entity.UniversityEntity;
+import com.example.beliemeserver.data.repository.UniversityRepository;
+import com.example.beliemeserver.model.dao.UniversityDao;
+import com.example.beliemeserver.model.dto.UniversityDto;
+import com.example.beliemeserver.model.exception.ConflictException;
+import com.example.beliemeserver.model.exception.DataException;
+import com.example.beliemeserver.model.exception.NotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UniversityDaoImpl implements UniversityDao {
+    final
+    UniversityRepository universityRepository;
+
+    public UniversityDaoImpl(UniversityRepository universityRepository) {
+        this.universityRepository = universityRepository;
+    }
+
+    @Override
+    public List<UniversityDto> getAllUniversitiesData() throws DataException {
+        List<UniversityDto> output = new ArrayList<>();
+
+        for (UniversityEntity universityEntity : universityRepository.findAll()) {
+            output.add(universityEntity.toUniversityDto());
+        }
+        return output;
+    }
+
+    @Override
+    public UniversityDto getUniversityByCodeData(String code) throws DataException, NotFoundException {
+        UniversityEntity targetUniversity = getUniversityEntityByCode(code);
+        return targetUniversity.toUniversityDto();
+    }
+
+    @Override
+    public UniversityDto addUniversityData(UniversityDto newUniversity) throws DataException, ConflictException {
+        if(universityRepository.existsByCode(newUniversity.getCode())) {
+            throw new ConflictException();
+        }
+
+        UniversityEntity newUniversityEntity = new UniversityEntity(
+                newUniversity.getCode(),
+                newUniversity.getName(),
+                newUniversity.getApiUrl()
+        );
+
+        UniversityEntity savedUniversityEntity = universityRepository.save(newUniversityEntity);
+        return savedUniversityEntity.toUniversityDto();
+    }
+
+    @Override
+    public UniversityDto updateUniversityData(String code, UniversityDto newUniversity) throws DataException, NotFoundException {
+        UniversityEntity targetUniversity = getUniversityEntityByCode(code);
+
+        UniversityEntity newUniversityEntity = new UniversityEntity(
+                targetUniversity.getId(),
+                newUniversity.getCode(),
+                newUniversity.getName(),
+                newUniversity.getApiUrl()
+        );
+
+        UniversityEntity savedUniversityEntity = universityRepository.save(newUniversityEntity);
+        return savedUniversityEntity.toUniversityDto();
+    }
+
+    private UniversityEntity getUniversityEntityByCode(String code) throws NotFoundException {
+        UniversityEntity targetUniversity = universityRepository.findByCode(code).orElse(null);
+        if(targetUniversity == null) {
+            throw new NotFoundException();
+        }
+        return targetUniversity;
+    }
+}
