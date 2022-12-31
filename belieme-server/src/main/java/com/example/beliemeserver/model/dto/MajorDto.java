3d7b5fef8a3c9b9b73be46084e46a247f45a7566
ESeokhwan <huan99@hanmail.net>
2022-12-31 23:23:58 +0900

    test: add test for major dao

--- belieme-server/src/main/java/com/example/beliemeserver/model/dto/MajorDto.java
+++ belieme-server/src/main/java/com/example/beliemeserver/model/dto/MajorDto.java
@@ -4,10 +4,11 @@ import lombok.*;
 
 @Getter
 @Setter
+@ToString
+@EqualsAndHashCode
 @AllArgsConstructor
 public class MajorDto {
     private UniversityDto university;
-
     @NonNull
     private String code;
 }
--- belieme-server/src/test/resources/data.sql
+++ belieme-server/src/test/resources/data.sql
@@ -1,3 +1,12 @@
 insert into university(code, name, api_url) values ('HYU', '한양대학교', 'https://api.hanyang.ac.kr/oauth/authorize/');
 insert into university(code, name, api_url) values ('CKU', '가톨릭관동대학교', null);
 insert into university(code, name, api_url) values ('SNU', '서울대학교', null);
+
+insert into major(university_id, code) values (1, 'FH04067');
+insert into major(university_id, code) values (1, 'FH04068');
+insert into major(university_id, code) values (1, 'FH04069');
+insert into major(university_id, code) values (2, 'TEST');
+insert into major(university_id, code) values (1, 'TEST');
+insert into major(university_id, code) values (2, 'A68');
+insert into major(university_id, code) values (2, 'A69');
+insert into major(university_id, code) values (2, 'A70');
