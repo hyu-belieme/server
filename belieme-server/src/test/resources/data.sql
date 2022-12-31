dcdf604900eaf37fe27bb630bcf99d52ec22b3c5
ESeokhwan <huan99@hanmail.net>
2022-12-31 17:57:47 +0900

    test: add logic that initialize university data on test db

---
+++ belieme-server/src/test/resources/data.sql
@@ -0,0 +1,3 @@
+insert into university(code, name, api_url) values ('HYU', '한양대학교', 'https://api.hanyang.ac.kr/oauth/authorize/');
+insert into university(code, name, api_url) values ('CKU', '가톨릭관동대학교', null);
+insert into university(code, name, api_url) values ('SNU', '서울대학교', null);
