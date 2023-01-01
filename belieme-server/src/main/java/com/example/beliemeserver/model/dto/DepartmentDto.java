765aede180665862ec1be66b333fb2bde5093282
ESeokhwan <huan99@hanmail.net>
2023-01-01 18:42:02 +0900

    refactor: remove Constructor Annotations and add Constructor on DepartmentDto

--- belieme-server/src/main/java/com/example/beliemeserver/model/dto/DepartmentDto.java
+++ belieme-server/src/main/java/com/example/beliemeserver/model/dto/DepartmentDto.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class DepartmentDto {
     private UniversityDto university;
 
@@ -17,7 +18,21 @@ public class DepartmentDto {
     @NonNull
     private String name;
 
-    private final List<MajorDto> baseMajors = new ArrayList<>();
+    private final List<MajorDto> baseMajors;
+
+    public DepartmentDto(UniversityDto university, String code, String name) {
+        this.university = university;
+        this.code = code;
+        this.name = name;
+        this.baseMajors = new ArrayList<>();
+    }
+
+    public DepartmentDto(UniversityDto university, String code, String name, List<MajorDto> baseMajors) {
+        this.university = university;
+        this.code = code;
+        this.name = name;
+        this.baseMajors = new ArrayList<>(baseMajors);
+    }
 
     public void addBaseMajor(MajorDto major) {
         baseMajors.add(major);
