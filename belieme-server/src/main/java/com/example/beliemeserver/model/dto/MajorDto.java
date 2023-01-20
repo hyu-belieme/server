8581c87cb63f8114f9554ee7c9fbbf51040f9089
ESeokhwan <huan99@hanmail.net>
2023-01-20 10:29:46 +0900

    feat: userToken 검증 메소드 추가

--- belieme-server/src/main/java/com/example/beliemeserver/model/dto/MajorDto.java
+++ belieme-server/src/main/java/com/example/beliemeserver/model/dto/MajorDto.java
@@ -13,6 +13,24 @@ public record MajorDto(@NonNull UniversityDto university, @NonNull String code) 
         return new MajorDto(university, code);
     }
 
+    public boolean matchUniqueKey(String universityCode, String majorCode) {
+        if(universityCode == null || majorCode == null) {
+            return false;
+        }
+        return universityCode.equals(this.university().code())
+                && majorCode.equals(this.code());
+    }
+
+    public boolean matchUniqueKey(MajorDto oth) {
+        if(oth == null) {
+            return false;
+        }
+        String universityCode = oth.university().code();
+        String majorCode = oth.code();
+        return universityCode.equals(this.university().code())
+                && majorCode.equals(this.code());
+    }
+
     @Override
     public String toString() {
         if(this.equals(nestedEndpoint)) {
