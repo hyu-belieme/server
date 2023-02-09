a4c02cb639dfb4760e307a73dae50d09ca8c9328
ESeokhwan <huan99@hanmail.net>
2023-02-09 16:19:31 +0900

    feat: university를 등록 변경하는 기능 제거

--- belieme-server/src/main/java/com/example/beliemeserver/model/service/UniversityService.java
+++ belieme-server/src/main/java/com/example/beliemeserver/model/service/UniversityService.java
@@ -36,32 +36,4 @@ public class UniversityService extends BaseService {
         checkDeveloperPermission(userToken);
         return universityDao.getByIndex(universityCode);
     }
-
-    public UniversityDto create(
-            @NonNull String userToken, @NonNull String universityCode,
-            @NonNull String name, String apiUrl
-    ) {
-        checkDeveloperPermission(userToken);
-
-        UniversityDto newUniversity = new UniversityDto(universityCode, name, apiUrl);
-        return universityDao.create(newUniversity);
-    }
-
-    public UniversityDto update(
-            @NonNull String userToken,
-            @NonNull String universityCode,
-            String newUniversityCode, String newName, String newApiUrl
-    ) {
-        checkDeveloperPermission(userToken);
-
-        UniversityDto oldUniversity = universityDao.getByIndex(universityCode);
-        if(newUniversityCode == null && newName == null && newApiUrl == null) return oldUniversity;
-
-        if(newUniversityCode == null) newUniversityCode = oldUniversity.code();
-        if(newName == null) newName = oldUniversity.name();
-        if(newApiUrl == null) newApiUrl = oldUniversity.apiUrl();
-        UniversityDto newUniversity = new UniversityDto(newUniversityCode, newName, newApiUrl);
-
-        return universityDao.update(universityCode, newUniversity);
-    }
 }
