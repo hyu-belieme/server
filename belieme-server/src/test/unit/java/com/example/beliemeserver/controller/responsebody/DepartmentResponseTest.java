6c86fb9f7b2b470f33dea2513e847b4dbd174397
ESeokhwan <huan99@hanmail.net>
2023-02-14 11:25:40 +0900

    test: departmentResponse json serialize 테스트 추가

---
+++ belieme-server/src/test/unit/java/com/example/beliemeserver/controller/responsebody/DepartmentResponseTest.java
@@ -0,0 +1,33 @@
+package com.example.beliemeserver.controller.responsebody;
+
+import com.example.beliemeserver.model.dto.DepartmentDto;
+import com.example.beliemeserver.util.RandomGetter;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.io.IOException;
+
+
+public class DepartmentResponseTest extends BaseResponseTest {
+    @RepeatedTest(10)
+    @DisplayName("[-]_[`department json serialization` 테스트]_[-]")
+    public void departmentJsonSerializationTest() throws IOException, ParseException {
+        RandomGetter<DepartmentDto> deptGetter = new RandomGetter<>(stub.ALL_DEPTS);
+        DepartmentDto dept = deptGetter.randomSelect();
+
+        JSONObject json = makeJsonObject(DepartmentResponse.from(dept));
+        basicDeptJsonCmpAssertions(json, dept);
+    }
+
+    @RepeatedTest(10)
+    @DisplayName("[-]_[`university` 제외한 `department json serialization` 테스트]_[-]")
+    public void departmentWithoutUniversityJsonSerializationTest() throws IOException, ParseException {
+        RandomGetter<DepartmentDto> deptGetter = new RandomGetter<>(stub.ALL_DEPTS);
+        DepartmentDto dept = deptGetter.randomSelect();
+
+        JSONObject json = makeJsonObject(DepartmentResponse.from(dept).withoutUniversity());
+        deptWithoutUnivJsonCmpAssertions(json, dept);
+    }
+}
