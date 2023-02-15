9e5e35af0495e5bd8e0b12f13d711f971a30457b
ESeokhwan <huan99@hanmail.net>
2023-02-15 12:17:59 +0900

    feat: 특정 item에 대한 대여 신청을 보내는 api 추가

--- belieme-server/src/main/java/com/example/beliemeserver/controller/api/HistoryApiController.java
+++ belieme-server/src/main/java/com/example/beliemeserver/controller/api/HistoryApiController.java
@@ -104,6 +104,21 @@ public class HistoryApiController {
         return ResponseEntity.ok(response);
     }
 
+    @PatchMapping("/stuffs/{stuff-name}/items/{item-num}/reserve")
+    public ResponseEntity<HistoryResponse> reserveItem(
+            @RequestHeader("user-token") String userToken,
+            @PathVariable("university-code") String universityCode,
+            @PathVariable("department-code") String departmentCode,
+            @PathVariable("stuff-name") String stuffName,
+            @PathVariable("item-num") int itemNum
+    ) {
+        HistoryDto historyDto = historyService.createReservation(
+                userToken, universityCode, departmentCode,
+                stuffName, itemNum);
+        HistoryResponse response = HistoryResponse.from(historyDto);
+        return ResponseEntity.ok(response);
+    }
+
     private List<HistoryResponse> toResponseList(List<HistoryDto> historyDtoList) {
         List<HistoryResponse> responseList = new ArrayList<>();
         for(HistoryDto dto : historyDtoList) {
