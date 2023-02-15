eb5d400e925bcf9af91f94dbdd3a3c90185bfa2a
ESeokhwan <huan99@hanmail.net>
2023-02-15 11:44:35 +0900

    feat: 특정 물품의 모든 Item 불러오는 api 추가

---
+++ belieme-server/src/main/java/com/example/beliemeserver/controller/api/ItemApiController.java
@@ -0,0 +1,41 @@
+package com.example.beliemeserver.controller.api;
+
+import com.example.beliemeserver.controller.responsebody.ItemResponse;
+import com.example.beliemeserver.model.dto.ItemDto;
+import com.example.beliemeserver.model.service.ItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/universities/{university-code}/departments/{department-code}/stuffs/{stuff-name}")
+public class ItemApiController {
+    private final ItemService itemService;
+
+    public ItemApiController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/items")
+    public ResponseEntity<List<ItemResponse>> getAllItemsOfStuff(
+            @RequestHeader("user-token") String userToken,
+            @PathVariable("university-code") String universityCode,
+            @PathVariable("department-code") String departmentCode,
+            @PathVariable("stuff-name") String stuffName
+    ) {
+        List<ItemDto> itemDtoList = itemService.getListByStuff(
+                userToken, universityCode, departmentCode, stuffName);
+        List<ItemResponse> responseList = toResponseList(itemDtoList);
+        return ResponseEntity.ok(responseList);
+    }
+
+    private List<ItemResponse> toResponseList(List<ItemDto> itemDtoList) {
+        List<ItemResponse> responseList = new ArrayList<>();
+        for(ItemDto dto : itemDtoList) {
+            responseList.add(ItemResponse.from(dto));
+        }
+        return responseList;
+    }
+}
