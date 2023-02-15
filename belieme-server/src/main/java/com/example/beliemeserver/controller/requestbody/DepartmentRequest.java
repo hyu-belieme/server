d7ca483b87cd8d33019893bc8629856bfcd9e4c3
ESeokhwan <huan99@hanmail.net>
2023-02-15 10:45:33 +0900

    feat: department 관련 api 추가

---
+++ belieme-server/src/main/java/com/example/beliemeserver/controller/requestbody/DepartmentRequest.java
@@ -0,0 +1,15 @@
+package com.example.beliemeserver.controller.requestbody;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class DepartmentRequest {
+    @JsonProperty("code") String code;
+    @JsonProperty("name") String name;
+    @JsonProperty("base-majors") List<String> baseMajors;
+}
