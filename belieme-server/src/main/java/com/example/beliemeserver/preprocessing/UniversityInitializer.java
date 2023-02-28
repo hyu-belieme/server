21d403aa8c6669e42c7e37f1a273fe4b1c330643
ESeokhwan <huan99@hanmail.net>
2023-02-28 17:17:39 +0900

    feat: 사전에 코드상으로 등록한 University를 DB에 저장하는 초기화 로직 추가

---
+++ belieme-server/src/main/java/com/example/beliemeserver/preprocessing/UniversityInitializer.java
@@ -0,0 +1,21 @@
+package com.example.beliemeserver.preprocessing;
+
+import com.example.beliemeserver.model.service.UniversityService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)
+@Component
+public class UniversityInitializer implements CommandLineRunner {
+    private final UniversityService universityService;
+
+    public UniversityInitializer(UniversityService universityService) {
+        this.universityService = universityService;
+    }
+
+    @Override
+    public void run(String... args) {
+        universityService.initUniversity();
+    }
+}
