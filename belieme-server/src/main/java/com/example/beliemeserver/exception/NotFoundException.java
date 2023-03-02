8e938908d844ba5775fdd25ec47a779f7f2a6a0d
ESeokhwan <huan99@hanmail.net>
2023-03-02 10:48:25 +0900

    feat: NotFoundException의 message 수정

--- belieme-server/src/main/java/com/example/beliemeserver/exception/NotFoundException.java
+++ belieme-server/src/main/java/com/example/beliemeserver/exception/NotFoundException.java
@@ -2,8 +2,7 @@ package com.example.beliemeserver.exception;
 
 public class NotFoundException extends InternalServerException {
     private String KR_MESSAGE() {
-        return "요청된 리소스를 찾을 수 없습니다. " +
-                "요청한 URL 혹은 Request Body를 다시한번 확인하고 요청하여 주십시오.";
+        return "요청된 리소스를 찾을 수 없습니다. 요청을 다시 한번 확인하여 주십시오.";
     }
 
     public NotFoundException() {
