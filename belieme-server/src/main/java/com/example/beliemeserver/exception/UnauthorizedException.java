930fe94cf6754d374499b8b4f91a170f4d32bdc8
ESeokhwan <huan99@hanmail.net>
2023-03-02 10:10:14 +0900

    feat: UnauthoirzedException의 message 정의

--- belieme-server/src/main/java/com/example/beliemeserver/exception/UnauthorizedException.java
+++ belieme-server/src/main/java/com/example/beliemeserver/exception/UnauthorizedException.java
@@ -1,7 +1,11 @@
 package com.example.beliemeserver.exception;
 
 public class UnauthorizedException extends InternalServerException {
+    private String KR_MESSAGE() {
+        return "액세스가 거부되었습니다. 유효한 토큰을 통해 접근해 주십시오.";
+    }
     public UnauthorizedException() {
+        setMessage(KR_MESSAGE());
     }
 
     public UnauthorizedException(String message) {
