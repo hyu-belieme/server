3ab6acd231fdc13b4b6d125cd8efa264f6558969
ESeokhwan <huan99@hanmail.net>
2023-03-02 10:36:20 +0900

    feat: ConflictException의 message 정의

--- belieme-server/src/main/java/com/example/beliemeserver/exception/ConflictException.java
+++ belieme-server/src/main/java/com/example/beliemeserver/exception/ConflictException.java
@@ -1,10 +1,12 @@
 package com.example.beliemeserver.exception;
 
 public class ConflictException extends InternalServerException {
-    public ConflictException() {
+    private String KR_MESSAGE() {
+        return "현재 리소스 상태와 충돌로 인해 요청한 작업을 완료할 수 없습니다. " +
+                "기존의 리소스 상태를 다시 한번 확인하고 요청하여 주십시오.";
     }
 
-    public ConflictException(String message) {
-        super(message);
+    public ConflictException() {
+        setMessage(KR_MESSAGE());
     }
 }
