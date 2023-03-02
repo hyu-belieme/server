ebc18cdaef132afc0e866a55a3f308bb3c7954c1
ESeokhwan <huan99@hanmail.net>
2023-03-02 10:42:55 +0900

    feat: NotFoundException의 message 정의

--- belieme-server/src/main/java/com/example/beliemeserver/exception/NotFoundException.java
+++ belieme-server/src/main/java/com/example/beliemeserver/exception/NotFoundException.java
@@ -1,11 +1,12 @@
 package com.example.beliemeserver.exception;
 
 public class NotFoundException extends InternalServerException {
-    public NotFoundException() {
-        super();
+    private String KR_MESSAGE() {
+        return "요청된 리소스를 찾을 수 없습니다. " +
+                "요청한 URL 혹은 Request Body를 다시한번 확인하고 요청하여 주십시오.";
     }
 
-    public NotFoundException(String message) {
-        super(message);
+    public NotFoundException() {
+        setMessage(KR_MESSAGE());
     }
 }
