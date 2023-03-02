8f3c128583e074c80cbd56f9e755f06fe7c0050f
ESeokhwan <huan99@hanmail.net>
2023-03-02 09:53:51 +0900

    feat: ExpiredTokenException의 message 정의

--- belieme-server/src/main/java/com/example/beliemeserver/exception/ExpiredTokenException.java
+++ belieme-server/src/main/java/com/example/beliemeserver/exception/ExpiredTokenException.java
@@ -1,10 +1,9 @@
 package com.example.beliemeserver.exception;
 
 public class ExpiredTokenException extends InternalServerException {
-    public ExpiredTokenException() {
-    }
+    private static final String KR_MESSAGE = "해당 토큰이 만료되었습니다.";
 
-    public ExpiredTokenException(String message) {
-        super(message);
+    public ExpiredTokenException() {
+        setMessage(KR_MESSAGE);
     }
 }
