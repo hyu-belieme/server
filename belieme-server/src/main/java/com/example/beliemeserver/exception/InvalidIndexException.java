f891a72eec3391acbbeb8c97b19dc48ec695628f
ESeokhwan <huan99@hanmail.net>
2023-03-02 11:01:58 +0900

    feat: InvalidIndexException의 message 수정

--- belieme-server/src/main/java/com/example/beliemeserver/exception/InvalidIndexException.java
+++ belieme-server/src/main/java/com/example/beliemeserver/exception/InvalidIndexException.java
@@ -1,11 +1,12 @@
 package com.example.beliemeserver.exception;
 
 public class InvalidIndexException extends InternalServerException {
-    public InvalidIndexException() {
-        super();
+    private String KR_MESSAGE() {
+        return "제공된 인덱스가 잘못되었습니다. " +
+                "요청을 확인하고 유효한 인덱스를 제공하여 다시 시도하여 주십시오.";
     }
 
-    public InvalidIndexException(String message) {
-        super(message);
+    public InvalidIndexException() {
+        setMessage(KR_MESSAGE());
     }
 }
