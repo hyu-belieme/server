90d5a9dc886a6492794f702e67cc02901afa67f1
ESeokhwan <huan99@hanmail.net>
2023-03-02 10:48:05 +0900

    feat: BadGateWayException의 message 정의

--- belieme-server/src/main/java/com/example/beliemeserver/exception/BadGateWayException.java
+++ belieme-server/src/main/java/com/example/beliemeserver/exception/BadGateWayException.java
@@ -1,7 +1,13 @@
 package com.example.beliemeserver.exception;
 
 public class BadGateWayException extends InternalServerException {
+    private String KR_MESSAGE() {
+        return "`upstream server` 또는 `gateway`에서 잘못된 응답을 수신했습니다. " +
+                "나중에 다시 시도하거나 요청을 다시 한번 확인하여 주십시오.";
+    }
+
     public BadGateWayException() {
+        setMessage(KR_MESSAGE());
     }
 
     public BadGateWayException(String message) {
