7d1503bfaf67481964a9caab0c8ff3d19ae2b324
ESeokhwan <huan99@hanmail.net>
2023-05-15 21:07:34 +0900

    feat: 기존 repository directory로 감싸고 config-server directory 추가

---
+++ config-server/src/main/java/com/belieme/configserver/BeliemeConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.belieme.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeliemeConfigServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BeliemeConfigServerApplication.class, args);
+  }
+
+}
---
+++ config-server/src/test/java/com/belieme/configserver/BeliemeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belieme.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeliemeConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
