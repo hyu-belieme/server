0467f3aecfc8f4b5da1cc268dc52975bd257bae7
ESeokhwan <huan99@hanmail.net>
2023-05-15 23:10:53 +0900

    feat: private github repository에 config server 연동

--- config-server/src/main/java/com/belieme/configserver/BeliemeConfigServerApplication.java
+++ config-server/src/main/java/com/belieme/configserver/BeliemeConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.belieme.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class BeliemeConfigServerApplication {
 
   public static void main(String[] args) {
