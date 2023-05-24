ab396e11d2210f8d10ce9cecd94b278f7c4315d6
ESeokhwan <huan99@hanmail.net>
2023-05-24 12:57:30 +0900

    [버그] requestBody를 제대로 parsing 하지 못하는 버그 수정

--- api-server/src/main/java/com/belieme/apiserver/web/requestbody/DepartmentRequest.java
+++ api-server/src/main/java/com/belieme/apiserver/web/requestbody/DepartmentRequest.java
@@ -2,6 +2,7 @@ package com.belieme.apiserver.web.requestbody;
 
 import com.belieme.apiserver.web.requestbody.validatemarker.DepartmentCreateValidationGroup;
 import com.belieme.apiserver.web.requestbody.validatemarker.DepartmentUpdateValidationGroup;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -14,6 +15,7 @@ import lombok.Setter;
 public class DepartmentRequest {
 
   @NotNull(message = "{message.error.badRequest.notNull}", groups = DepartmentCreateValidationGroup.class)
+  @JsonProperty("universityId")
   String universityId;
 
   @Size(min = 1, max = 30, message = "{message.error.badRequest.outOfSize}", groups = {
@@ -21,8 +23,10 @@ public class DepartmentRequest {
       DepartmentUpdateValidationGroup.class}) @Pattern(regexp = "[\\p{L}\\d() ]+", message = "{message.error.badRequest.containsNonLetter}", groups = {
       DepartmentCreateValidationGroup.class,
       DepartmentUpdateValidationGroup.class}) @NotNull(message = "{message.error.badRequest.notNull}", groups = DepartmentCreateValidationGroup.class)
+  @JsonProperty("name")
   String name;
 
   @NotNull(message = "{message.error.badRequest.notNull}", groups = DepartmentCreateValidationGroup.class)
+  @JsonProperty("baseMajors")
   List<String> baseMajors;
 }
